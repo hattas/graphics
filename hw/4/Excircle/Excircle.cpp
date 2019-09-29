6e68f36e9ebc63c3c378039e6583712b53e990b0
John Hattas <johnhattas@gmail.com>
2019-09-28 20:18:33 -0500

    draw bisectors

--- hw/4/Excircle/Excircle.cpp
+++ hw/4/Excircle/Excircle.cpp
@@ -17,7 +17,7 @@ std::ostream& operator<< (std::ostream& out, Point const& p) {
 }
 
 // constants
-const int screenW = 400, screenH = 400;
+const int screenW = 600, screenH = 600;
 const int numPoints = 3;
 const double PI = 3.1415926535897932384626433832795;
 
@@ -35,7 +35,7 @@ void init(void) {
 	// open gl init
 	glClearColor(1, 1, 1, 0);
 	glColor3f(0, 0, 0);
-	glPointSize(4);
+	glPointSize(5);
 	glLineWidth(2);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -86,6 +86,16 @@ void drawCircle(double x, double y, double radius) {
 	glEnd();
 }
 
+void drawBisectors() {
+	glColor3d(1, 0.7, 0.7);
+	for (int i = 0; i < numPoints; i++) {
+		glBegin(GL_LINE_STRIP);
+		glVertex2d(midPoints[i].x, midPoints[i].y);
+		glVertex2d(bisectPoints[i].x, bisectPoints[i].y);
+		glEnd();
+	}
+}
+
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 
@@ -95,10 +105,11 @@ void display(void) {
 		return;
 	}
 
-	
 	drawCenter();
 	drawCircle(center.x, center.y, radius);
 	drawPoints();
+	drawBisectors();
+	drawCenter();
 	drawMidPoints();
 
 
@@ -129,10 +140,6 @@ void calculateSlopes() {
 	}
 }
 
-void calculateBisectors() {
-	return;
-}
-
 Point intersection(Point p1, double m1, Point p2, double m2) {
 	double num, den, x, y;
 	num = m1 * p1.x - m2 * p2.x - p1.y + p2.y;
@@ -143,9 +150,35 @@ Point intersection(Point p1, double m1, Point p2, double m2) {
 	return p;
 }
 
+double distance(Point p1, Point p2) {
+	return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+}
+
+void calculateBisectors() {
+	// find intersection between each midpoint and other 2 pairs of lines
+	// choose smaller line to show bisector
+	int i, j, k;
+	Point p1, p2;
+	double d1, d2;
+	for (i = 0; i < numPoints; i++) {
+		j = (i + 1) % numPoints;
+		k = (i + 2) % numPoints;
+		p1 = intersection(midPoints[i], perpendicularSlopes[i], points[j], slopes[j]);
+		p2 = intersection(midPoints[i], perpendicularSlopes[i], points[k], slopes[k]);
+		d1 = distance(midPoints[i], p1);
+		d2 = distance(midPoints[i], p2);
+		if (d1 < d2)
+			bisectPoints[i] = p1;
+		else
+			bisectPoints[i] = p2;
+	}
+
+	return;
+}
+
 void calculateCenter() {
 	center = intersection(midPoints[0], perpendicularSlopes[0], midPoints[1], perpendicularSlopes[1]);
-	radius = sqrt(pow(center.x - points[0].x, 2) + pow(center.y - points[0].y, 2));
+	radius = distance(center, points[0]);
 	cout << "center = " << center << "\n";
 	cout << "radius = " << radius << "\n";
 }
@@ -181,7 +214,7 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(screenW, screenH);
-	glutInitWindowPosition(500, 500);
+	glutInitWindowPosition(500, 300);
 	glutCreateWindow("HW 4 Excircle");
 
 	glutDisplayFunc(display);
