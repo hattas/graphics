c3e9c477d4be7298773988e7cf0e1dff4e5bf3c0
John Hattas <johnhattas@gmail.com>
2019-09-28 20:07:20 -0500

    circle draws connecting the 3 points

--- hw/4/Excircle/Excircle.cpp
+++ hw/4/Excircle/Excircle.cpp
@@ -4,20 +4,32 @@
 #include <GL/glut.h>
 #include <fstream>
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 struct Point {
 	double x, y;
 };
 
+std::ostream& operator<< (std::ostream& out, Point const& p) {
+	out << "(" << p.x << ", " << p.y << ")";
+	return out;
+}
+
 // constants
 const int screenW = 400, screenH = 400;
 const int numPoints = 3;
+const double PI = 3.1415926535897932384626433832795;
 
 // globals
 Point points[numPoints];
 Point midPoints[numPoints];
+double slopes[numPoints];
+double perpendicularSlopes[numPoints];
+Point bisectPoints[numPoints];
 int numPointsSelected = 0;
+Point center;
+double radius;
 
 void init(void) {
 	// open gl init
@@ -29,7 +41,6 @@ void init(void) {
 	glLoadIdentity();
 	gluOrtho2D(0, screenW, 0, screenH);
 }
-
 
 Point lerp(Point a, Point b, double t) {
 	Point c = { a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t };
@@ -60,17 +71,36 @@ void drawMidPoints() {
 	glEnd();
 }
 
+void drawCenter() {
+	glColor3d(0, 0, 1);
+	glBegin(GL_POINTS);
+	glVertex2d(center.x, center.y);
+	glEnd();
+}
+
+void drawCircle(double x, double y, double radius) {
+	glColor3d(0, 1, 0);
+	glBegin(GL_LINE_LOOP);
+	for (double i = 0; i < 2 * PI; i += PI / 40)
+		glVertex2f(x + cos(i) * radius, y + sin(i) * radius);
+	glEnd();
+}
+
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	drawPoints();
 	if (numPointsSelected < numPoints) {
+		drawPoints();
 		glutSwapBuffers();
 		return;
 	}
-	else {
-		drawMidPoints();
-	}
+
+	
+	drawCenter();
+	drawCircle(center.x, center.y, radius);
+	drawPoints();
+	drawMidPoints();
+
 
 	glutSwapBuffers();
 	glutPostRedisplay();
@@ -81,18 +111,49 @@ void calculateMidpoints() {
 	for (i = 0; i < numPoints; i++) {
 		j = (i + 1) % numPoints;
 		midPoints[i] = lerp(points[i], points[j], 0.5);
+		cout << "midPoints[" << i << "] = " << midPoints[i] << "\n";
 	}
 }
 
-int checkIfPointsAreCollinear() {
-	double slopes[numPoints - 1];
-	for (int i = 0; i < numPoints - 1; i++) {
-		if (points[i + 1].x - points[i].x == 0)
+void calculateSlopes() {
+	int i, j;
+	for (i = 0; i < numPoints; i++) {
+		j = (i + 1) % numPoints;
+		if (points[j].x - points[i].x == 0)
 			slopes[i] = 0;
 		else
-			slopes[i] = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
+			slopes[i] = (points[j].y - points[i].y) / (points[j].x - points[i].x);
+
+		perpendicularSlopes[i] = -1 / slopes[i];
+		cout << "slopes[" << i << "] = " << slopes[i] << " " << "perpendicularSlopes[" << i << "] = " << perpendicularSlopes[i] << "\n";
 	}
-	return slopes[1] == slopes[0];
+}
+
+void calculateBisectors() {
+	return;
+}
+
+Point intersection(Point p1, double m1, Point p2, double m2) {
+	double num, den, x, y;
+	num = m1 * p1.x - m2 * p2.x - p1.y + p2.y;
+	den = m1 - m2;
+	x = num / den;
+	y = m1 * (x - p1.x) + p1.y;
+	Point p{ x, y };
+	return p;
+}
+
+void calculateCenter() {
+	center = intersection(midPoints[0], perpendicularSlopes[0], midPoints[1], perpendicularSlopes[1]);
+	radius = sqrt(pow(center.x - points[0].x, 2) + pow(center.y - points[0].y, 2));
+	cout << "center = " << center << "\n";
+	cout << "radius = " << radius << "\n";
+}
+
+void calculateStuff() {
+	calculateMidpoints();
+	calculateBisectors();
+	calculateCenter();
 }
 
 void myMouse(int button, int state, int x, int y) {
@@ -105,10 +166,11 @@ void myMouse(int button, int state, int x, int y) {
 			// if user drew all points, check if they are collinear
 			// if they are, reset
 			if (numPointsSelected == numPoints) {
-				if (checkIfPointsAreCollinear())
+				calculateSlopes();
+				if (slopes[0] == slopes[1])
 					numPointsSelected = 0;
 				else
-					calculateMidpoints();
+					calculateStuff();
 			}
 			glutPostRedisplay();
 		}
@@ -119,7 +181,7 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(screenW, screenH);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowPosition(500, 500);
 	glutCreateWindow("HW 4 Excircle");
 
 	glutDisplayFunc(display);
