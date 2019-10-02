07c8a59be26e452dab07568e2250480fc47ebafc
John Hattas <johnhattas@gmail.com>
2019-10-02 10:32:33 -0500

    pool ball sim changes

--- inclass/09-17_PoolBalls/Source.cpp
+++ inclass/09-17_PoolBalls/Source.cpp
@@ -13,38 +13,37 @@
 #define GREEN 104, 207, 54
 #define BROWN 240, 147, 43
 
+struct Ball {
+	int radius;
+	double x, y; // position
+	double vx, vy; // velocity
+	GLfloat angle; // angle ball is traveling in
+};
+
 // constants
 const int ballRadius = 20;
 const int screenW = 600;
 const int screenH = 400;
-const GLfloat maxForce = 10;
-const GLfloat minForce = 0;
-const GLfloat maxAngle = 90;
-const GLfloat minAngle = -90;
-const GLfloat forceIncrement = (maxForce - minForce) / 100.0;
-const GLfloat angleIncrement = (maxAngle - minAngle) / 180.0;
-const GLfloat guideLength = 100;
-const GLfloat PI = (GLfloat)3.1415926535897932384626433832795;
-const GLfloat veloThreshold = maxForce / 100;
+const double maxForce = 10;
+const double minForce = 0;
+const double maxAngle = 90;
+const double minAngle = -90;
+const double forceIncrement = (maxForce - minForce) / 100.0;
+const double angleIncrement = (maxAngle - minAngle) / 180.0;
+const int guideLength = 100;
+const double PI = 3.1415926535897932384626433832795;
+const double velocityThreshold = maxForce / 100;
+const double friction = 0.02;
 
 // globals
-GLfloat force = maxForce / 2;
-GLfloat launchAngle = 0;
-GLfloat launchAngleRadians = 0;
-GLfloat currentAngle = 0;
-
-GLfloat cueX = ballRadius;
-GLfloat cueY = screenH / 2;
-GLfloat eightX = screenW / 2;
-GLfloat eightY = screenH / 2;
-GLfloat cueVX = 0;
-GLfloat cueVY = 0;
-GLfloat eightVX = 0;
-GLfloat eightVY = 0;
-GLfloat cueDirX;
-GLfloat cueDirY;
-GLfloat friction = 0.2;
+double force = maxForce / 2;
+double launchAngle = 0;
+double launchAngleRadians = 0;
+double currentAngle = 0;
 boolean guideVisible = true;
+
+Ball cue = { ballRadius, ballRadius, screenH, 0, 0, 0 };
+Ball eight = { ballRadius, screenW / 2, screenH / 2, 0, 0, 0 };
 
 void color255(int r, int g, int b) {
 	glColor3f((GLfloat)(r / 255.0), (GLfloat)(g / 255.0), (GLfloat)(b / 255.0));
@@ -53,7 +52,7 @@ void color255(int r, int g, int b) {
 void myInit(void) {
 	glClearColor(0, 0, 0, 0);
 	color255(WHITE);
-	glPointSize(4);
+	glPointSize(1);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0.0, screenW, 0.0, screenH);
@@ -66,15 +65,15 @@ void drawForceBar(void) {
 	int bot = screenH / 8;
 	int top = screenH / 8 * 7;
 	glRecti(left, bot, right, top);
+
 	color255(RED);
 	double percentForce = force / maxForce;
-	glRecti(left, bot, right, (screenH / 8 * 7 - bot) * force / maxForce + bot);
-	glRecti(left, bot, right, (GLint)((top - bot) * percentForce + bot));
+	glRectf(left, bot, right, ((top - bot) * percentForce + bot));
 }
 
 void drawPoolTable(void) {
 	color255(GREEN);
-	glRecti(0, 0, screenW, screenH);
+	glRectf(0, 0, screenW, screenH);
 }
 
 void drawCircle(GLfloat x, GLfloat y, int radius) {
@@ -86,87 +85,85 @@ void drawCircle(GLfloat x, GLfloat y, int radius) {
 
 void drawCueBall(void) {
 	color255(CUE_WHITE);
-	drawCircle(cueX, cueY, ballRadius);
+	drawCircle(cue.x, cue.y, cue.radius);
 }
 
 void drawEightBall(void) {
 	color255(BLACK);
-	drawCircle(eightX, eightY, ballRadius);
+	drawCircle(eight.x, eight.y, eight.radius);
 }
 
 void drawGuide(void) {
-	color255(WHITE);
-	glBegin(GL_LINE_STRIP);
-	glVertex2f(cueX, cueY);
-	glVertex2f(cueX + guideLength * cos(launchAngleRadians), cueY + guideLength * sin(launchAngleRadians));
-	glEnd();
+	if (guideVisible) {
+		color255(WHITE);
+		glBegin(GL_LINE_STRIP);
+		glVertex2f(cue.x, cue.y);
+		glVertex2f(cue.x + guideLength * cos(launchAngleRadians), cue.y + guideLength * sin(launchAngleRadians));
+		glEnd();
+	}
+}
+
+boolean moveBalls(void) {
+	// adjust ball positions
+	cue.x += cue.vx;
+	cue.y += cue.vy;
+
+	// check for collisions with the walls
+	if ((cue.x - cue.radius) < 0) {
+		cue.x = cue.radius;
+		cue.vx = -cue.vx;
+	}
+	if ((cue.x + cue.radius) > screenW) {
+		cue.x = screenW - cue.radius;
+		cue.vx = -cue.vx;
+	}
+	if ((cue.y - cue.radius) < 0) {
+		cue.y = cue.radius;
+		cue.vy = -cue.vy;
+	}
+	if ((cue.y + cue.radius) > screenH) {
+		cue.y = screenH - cue.radius;
+		cue.vy = -cue.vy;
+	}
+
+	if (cue.vx != 0) {
+		currentAngle = atan(cue.vy / cue.vy);
+		cue.vx -= friction * abs(cos(currentAngle));
+		cue.vy -= friction * abs(sin(currentAngle));
+	}
+	else if (cue.vy > 0) {
+		cue.vy -= friction;
+	}
+
+	// check for small velocity and set to 0
+	GLfloat velocityMagnitude = abs(sqrt(cue.vx * cue.vx + cue.vy * cue.vy));
+	if (velocityMagnitude < velocityThreshold) {
+		cue.vx = 0;
+		cue.vy = 0;
+	}
+
+	return true;
 }
 
 void myDisplay(void) {
-	std::cout << cueVX << ' ' << cueVY << '\n';
-	// adjust ball positions
-	cueX += cueVX;
-	cueY += cueVY;
-
-	// check for collisions
-	if ((cueX - ballRadius) < 0) {
-		cueX = ballRadius;
-		cueVX = -cueVX;
-	}
-	if ((cueX + ballRadius) > screenW) {
-		cueX = screenW - ballRadius;
-		cueVX = -cueVX;
-	}
-	if ((cueY - ballRadius) < 0) {
-		cueY = ballRadius;
-		cueVY = -cueVY;
-	}
-	if ((cueY + ballRadius) > screenH) {
-		cueY = screenH - ballRadius;
-		cueVY = -cueVY;
-	}
-
-	// add friction
-	if (cueVX > 0)
-		cueDirX = 1;
-	else
-		cueDirX = -1;
-	if (cueVY > 0)
-		cueDirY = 1;
-	else
-		cueDirY = -1;
-	if (cueVX != 0) {
-		currentAngle = atan(cueVY / cueVX);
-		cueVX -= cueDirX * friction * abs(cos(currentAngle));
-		cueVY -= cueDirY * friction * abs(sin(currentAngle));
-	}
-	else {
-		cueVY -= cueDirY * friction;
-	}
-
-	// check for small velo and set to 0
-	if (abs(sqrt(cueVX * cueVX + cueVY * cueVY) < veloThreshold)) {
-		cueVX = 0;
-		cueVY = 0;
-	}
-
-
+	// set viewport to entire window and draw the force bar
 	glViewport(0, 0, screenW, screenH);
 	drawForceBar();
 
+	// set viewport to the pool table and draw the pool table scene
 	GLsizei tableH = 3 * screenH / 4;
 	GLsizei tableW = tableH / 2 * 3;
 	glViewport(3 * screenW / 16, screenH / 8, tableW, tableH);
 	drawPoolTable();
-	if (guideVisible)
-		drawGuide();
+	drawGuide();
 	drawCueBall();
 	drawEightBall();
 
 	glutSwapBuffers();
 
-	// if movement, update
-	glutPostRedisplay(); // loop constantly
+	// move the balls, and if they moved then redisplay
+	if (moveBalls())
+		glutPostRedisplay();
 }
 
 void mySpecialKeyboard(int theKey, int mouseX, int mouseY) {
@@ -200,8 +197,8 @@ void mySpecialKeyboard(int theKey, int mouseX, int mouseY) {
 void myKeyboard(unsigned char theKey, int mouseX, int mouseY) {
 	switch (theKey) {
 	case ' ':
-		cueVX = force * cos(launchAngleRadians);
-		cueVY = force * sin(launchAngleRadians);
+		cue.vx = force * cos(launchAngleRadians);
+		cue.vy = force * sin(launchAngleRadians);
 		guideVisible = false;
 		break;
 	default: break;
