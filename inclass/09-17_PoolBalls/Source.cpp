ba2a652a5aaa23c3a4c4fa578a916e8273b1f134
John Hattas <johnhattas@gmail.com>
2019-10-02 21:46:21 -0500

    working collisions between balls

--- inclass/09-17_PoolBalls/Source.cpp
+++ inclass/09-17_PoolBalls/Source.cpp
@@ -15,19 +15,16 @@
 
 struct Ball {
 	int radius;
-	double x, y; // position
+	double px, py; // position
 	double vx, vy; // velocity
-};
-
-struct Point {
-	double x, y;
+	double ax, ay;
 };
 
 // constants
 const int ballRadius = 20;
 const int screenW = 600;
 const int screenH = 400;
-const double maxForce = 10;
+const double maxForce = 30;
 const double minForce = 0;
 const double maxAngle = 90;
 const double minAngle = -90;
@@ -36,21 +33,21 @@ const double angleIncrement = (maxAngle - minAngle) / 180.0;
 const int guideLength = 100;
 const double PI = 3.1415926535897932384626433832795;
 const double velocityThreshold = maxForce / 100;
-const double friction = 0.02;
+const double friction = 0.01;
 
 // globals
-double force = maxForce / 2;
+double force = (maxForce + minForce) / 2;
 double launchAngle = 0;
 double launchAngleRadians = 0;
 double currentAngle = 0;
 boolean guideVisible = true;
 int del = 0;
 
-Ball cue = { ballRadius, ballRadius, screenH / 2, 0, 0 };
-Ball eight = { ballRadius, screenW / 2, screenH / 2, 0, 0 };
+Ball cue = { ballRadius, ballRadius, screenH / 2, 0, 0, 0, 0 };
+Ball eight = { ballRadius, screenW / 2, screenH / 2, 0, 0, 0, 0 };
 
 void print(Ball b) {
-	std::cout << "pos: (" << b.x << ", " << b.y << ") vel: (" << b.vx << ", " << b.vy << ")\n";
+	std::cout << "pos: (" << b.px << ", " << b.py << ") vel: (" << b.vx << ", " << b.vy << ") acc: (" << b.ax << ", " << b.ay << ")\n";
 }
 
 void color255(int r, int g, int b) {
@@ -76,7 +73,7 @@ void drawForceBar(void) {
 
 	color255(RED);
 	double percentForce = force / maxForce;
-	glRectd(left, bot, right, (((double) top - (double) bot) * percentForce + bot));
+	glRectd(left, bot, right, (((double)top - (double)bot) * percentForce + bot));
 }
 
 void drawPoolTable(void) {
@@ -93,77 +90,109 @@ void drawCircle(double x, double y, int radius) {
 
 void drawCueBall(void) {
 	color255(CUE_WHITE);
-	drawCircle(cue.x, cue.y, cue.radius);
+	drawCircle(cue.px, cue.py, cue.radius);
 }
 
 void drawEightBall(void) {
 	color255(BLACK);
-	drawCircle(eight.x, eight.y, eight.radius);
+	drawCircle(eight.px, eight.py, eight.radius);
 }
 
 void drawGuide(void) {
 	if (guideVisible) {
 		color255(WHITE);
 		glBegin(GL_LINE_STRIP);
-		glVertex2d(cue.x, cue.y);
-		glVertex2d(cue.x + guideLength * cos(launchAngleRadians), cue.y + guideLength * sin(launchAngleRadians));
+		glVertex2d(cue.px, cue.py);
+		glVertex2d(cue.px + guideLength * cos(launchAngleRadians), cue.py + guideLength * sin(launchAngleRadians));
 		glEnd();
 	}
 }
 
 int isCollision() {
 	double distance;
-	distance = sqrt(pow(cue.x - eight.x, 2) + pow(cue.y - eight.y, 2));
-	return distance <= ((double) cue.radius + (double) eight.radius);
+	distance = pow(cue.px - eight.px, 2) + pow(cue.py - eight.py, 2);
+	return distance <= pow((double)cue.radius + (double)eight.radius, 2);
 }
 
-void checkWallCollision(Ball *ball) {
-	if ((ball->x - ball->radius) < 0) {
-		ball->x = ball->radius;
+void checkWallCollision(Ball* ball) {
+	if ((ball->px - ball->radius) < 0) {
+		ball->px = ball->radius;
 		ball->vx = -ball->vx;
 	}
-	if ((ball->x + ball->radius) > screenW) {
-		ball->x = (double) screenW - (double) ball->radius;
+	if ((ball->px + ball->radius) > screenW) {
+		ball->px = (double)screenW - (double)ball->radius;
 		ball->vx = -ball->vx;
 	}
-	if ((ball->y - ball->radius) < 0) {
-		ball->y = ball->radius;
+	if ((ball->py - ball->radius) < 0) {
+		ball->py = ball->radius;
 		ball->vy = -ball->vy;
 	}
-	if ((ball->y + ball->radius) > screenH) {
-		ball->y = (double) screenH - (double) ball->radius;
+	if ((ball->py + ball->radius) > screenH) {
+		ball->py = (double)screenH - (double)ball->radius;
+		ball->vy = -ball->vy;
 	}
 }
 
-double velo(Ball *ball) {
+double velo(Ball* ball) {
 	return sqrt(pow(ball->vx, 2) + pow(ball->vy, 2));
 }
 
-Point ballCollision(Ball* moved, Ball* still) {
-	Point vector = { moved->x - still->x, moved->y - still->y };
-	double magnitude = sqrt(pow(vector.x, 2) + pow(vector.y, 2));
-	vector.x /= magnitude;
-	vector.y /= magnitude;
-	vector.x *= velo(moved);
-	vector.y *= velo(moved);
-	return vector;
-}
-
 int moveBalls(void) {
-	double slope;
+	double distance, overlap, nx, ny, kx, ky, p;
+	char collision;
 
-	cue.x += cue.vx;
-	cue.y += cue.vy;
-	eight.x += eight.vx;
-	eight.y += eight.vy;
+	// update ball positions
+	cue.ax = -cue.vx * friction;
+	cue.ay = -cue.vy * friction;
+	cue.px += cue.vx;
+	cue.py += cue.vy;
+	cue.vx += cue.ax;
+	cue.vy += cue.ay;
+	eight.ax = -eight.vx * friction;
+	eight.ay = -eight.vy * friction;
+	eight.px += eight.vx;
+	eight.py += eight.vy;
+	eight.vx += eight.ax;
+	eight.vy += eight.ay;
 
-	if (isCollision()) {
-		Point cueNewV = ballCollision(&cue, &eight);
-		Point eightNewV = ballCollision(&eight, &cue);
-		cue.vx = cueNewV.x;
-		cue.vy = cueNewV.y;
-		eight.vy = eightNewV.y;
-		eight.vy = eightNewV.y;
+	// cutoff velo close to 0
+	if (abs(cue.vx * cue.vx + cue.vy * cue.vy) < 0.01) {
+		cue.vx = 0;
+		cue.vy = 0;
+	}
+	if (abs(eight.vx * eight.vx + eight.vy * eight.vy) < 0.01) {
+		eight.vx = 0;
+		eight.vy = 0;
+	}
+
+	collision = isCollision();
+	
+	if (collision) {
+		// resolve static collision preventing balls from being inside each other
+		distance = sqrt(pow(cue.px - eight.px, 2) + pow(cue.py - eight.py, 2));
+		overlap = 0.5 * (distance - cue.radius - eight.radius);
+		
+		// move cue away from collision
+		cue.px -= overlap * (cue.px - eight.px) / distance;
+		cue.py -= overlap * (cue.py - eight.py) / distance;
+
+		// move eight ball away from collision
+		eight.px += overlap * (cue.px - eight.px) / distance;
+		eight.py += overlap * (cue.py - eight.py) / distance;
+
+		// resolve dynamic collision
+		// https://en.wikipedia.org/wiki/Elastic_collision
+		// section called Two-dimensional collision with two moving objects
+		distance = sqrt(pow(cue.px - eight.px, 2) + pow(cue.py - eight.py, 2));
+		nx = (eight.px - cue.px) / distance;
+		ny = (eight.py - cue.py) / distance;
+		kx = cue.vx - eight.vx;
+		ky = cue.vy - eight.vy;
+		p = nx * kx + ny * ky;
+		cue.vx = cue.vx - p * nx;
+		cue.vy = cue.vy - p * ny;
+		eight.vx = eight.vx + p * nx;
+		eight.vy = eight.vy + p * ny;
 	}
 
 	checkWallCollision(&cue);
@@ -227,6 +256,24 @@ void myKeyboard(unsigned char theKey, int mouseX, int mouseY) {
 		cue.vx = force * cos(launchAngleRadians);
 		cue.vy = force * sin(launchAngleRadians);
 		guideVisible = false;
+		break;
+	case 'r':
+		cue.px = cue.radius;
+		cue.py = screenH / 2;
+		cue.vx = 0;
+		cue.vy = 0;
+		cue.ax = 0;
+		cue.ay = 0;
+		eight.px = screenW / 2;
+		eight.py = screenH / 2;
+		eight.vx = 0;
+		eight.vy = 0;
+		eight.ax = 0;
+		eight.ay = 0;
+		launchAngle = 0;
+		launchAngleRadians = 0;
+		force = (maxForce + minForce) / 2;
+		guideVisible = true;
 		break;
 	default: break;
 	}
