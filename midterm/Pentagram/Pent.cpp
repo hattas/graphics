842c25730a1dc95e4df6e11e999f0df717c28cf2
John Hattas <johnhattas@gmail.com>
2019-10-14 00:13:51 -0500

    recursive pentagram

--- midterm/Pentagram/Pent.cpp
+++ midterm/Pentagram/Pent.cpp
@@ -2,22 +2,26 @@
 #include <gl/Gl.h>
 #include <gl/Glu.h>
 #include <gl/glut.h>
-#include <iostream.h>
 #include <math.h>
 
 // demo program to draw a rosette based on a 5-gon 
 
+const int NUM_POINTS = 5;
+const int NUM_LEVELS = 3;
+const int SCREEN_W = 400, SCREEN_H = 400;
+const double RADIUS_OUTER = SCREEN_W / 2.5;
+const double PI = 3.1415926536;
+const double PHI = 1.61803398875;
 
-class GLintPoint
-{
+
+class GLintPoint {
 public:
 	GLint x, y;
 };
 
 
 // point 2 class
-class Point2
-{
+class Point2 {
 public:
 	float x, y;
 	void set(float dx, float dy) { x = dx; y = dy; }
@@ -29,13 +33,11 @@ public:
 Point2 currPos;
 Point2 CP;
 
-void moveTo(Point2 p)
-{
+void moveTo(Point2 p) {
 	CP.set(p);
 }
 
-void lineTo(Point2 p)
-{
+void lineTo(Point2 p) {
 	glBegin(GL_LINES);
 	glVertex2f(CP.x, CP.y);
 	glVertex2f(p.x, p.y);
@@ -44,42 +46,46 @@ void lineTo(Point2 p)
 	CP.set(p);
 }
 
-
-void myInit(void)
-{
+void myInit(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(1.0, 0.0, 0.0, 0.0);
-	// background is red
-	glColor3f(0.0, 0.0, 1.0);
-	// drawing color is blue
+	glClearColor(1, 1, 1, 0);
+	glColor3d(6 / 255.0, 162 / 255.0, 232 / 255.0);
+	glLineWidth(1.5);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-SCREEN_W / 2, SCREEN_W / 2, -SCREEN_H / 2, SCREEN_H / 2);
 }
 
-
-void rosette(int N, float radius)
-{
+void rosette(int N, float radius, int flipped) {
 	Point2* pointlist = new Point2[N];
-	GLfloat theta = (2.0f * 3.1415926536) / N;
+	GLfloat theta = (2.0f * PI) / N;
+	double offset = 0;
 
-	for (int c = 0; c < N; c++)
-	{
-		pointlist[c].set(radius * sin(theta * c), radius * cos(theta * c));
+	if (flipped) {
+		offset = PI;
 	}
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
+
+
+	for (int c = 0; c < N; c++) {
+		pointlist[c].set(radius * sin(offset + theta * c), radius * cos(offset + theta * c));
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			moveTo(pointlist[i]);
 			lineTo(pointlist[j]);
 		}
 	}
 }
 
-void render()
-{
-	//this is the callback for displays
+void render() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glViewport(10, 10, 640, 480);
-	rosette(5, .66f);
+	double radius = RADIUS_OUTER;
+	int flipped = false;
+	for (int i = 0; i < NUM_LEVELS; i++) {
+		rosette(NUM_POINTS, radius, flipped);
+		flipped = !flipped;
+		radius /= (PHI * PHI);
+	}
 	glFlush();
 }
 
@@ -89,7 +95,7 @@ void main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glutInitWindowSize(640, 480);
+	glutInitWindowSize(SCREEN_W, SCREEN_H);
 	glutCreateWindow("Rosette");
 	glutDisplayFunc(render);// register the callback for the display function
 	myInit();
